ec10beb5ef12338eb0fe2676cf907475ea774203
k8tems <k8tems@live.com>
2021-03-18 05:30:57 +0900

    Rename class variable

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from main import *
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.F = (-158, -786)
+        self.facing_rng = (-158, -786)
 
     def read_event(self, fname):
         with open(fname) as f:
@@ -13,15 +13,15 @@ class TestCase(unittest.TestCase):
 
     def test_back_success(self):
         # 開幕3GCD目の方向指定成功してる月光
-        self.assertTrue(is_back(self.read_event('fixtures/success.json'), facing_rng=self.F))
+        self.assertTrue(is_back(self.read_event('fixtures/success.json'), facing_rng=self.facing_rng))
 
     def test_back_failure_short_width(self):
         # 上の`width`を少し縮めてプレイヤーがはみ出るようにする
-        self.assertFalse(is_back(self.read_event('fixtures/success.json'), facing_rng=self.F, width=5))
+        self.assertFalse(is_back(self.read_event('fixtures/success.json'), facing_rng=self.facing_rng, width=5))
 
     def test_back_failure_312(self):
         # 5:12頃の影潜りの方向指定失敗してるイベント
-        self.assertFalse(is_back(self.read_event('fixtures/failure.json'), facing_rng=self.F))
+        self.assertFalse(is_back(self.read_event('fixtures/failure.json'), facing_rng=self.facing_rng))
 
 
 if __name__ == '__main__':
