49d74f242fb971cbbff903affa7f097de83cf456
k8tems <k8tems@live.com>
2021-03-18 04:43:09 +0900

    Refactor tests

--- test.py
+++ test.py
@@ -4,23 +4,24 @@ from main import *
 
 
 class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.F = (-158, -786)
+
+    def read_event(self, fname):
+        with open(fname) as f:
+            return json.loads(f.read())
+
     def test_back_success(self):
         # 開幕3GCD目の方向指定成功してる月光
-        with open('fixtures/success.json') as f:
-            event = json.loads(f.read())
-        self.assertTrue(is_back(event, F=(-158, -786)))
+        self.assertTrue(is_back(self.read_event('fixtures/success.json'), F=self.F))
 
     def test_back_failure_short_width(self):
         # 上の`width`を少し縮めてプレイヤーがはみ出るようにする
-        with open('fixtures/success.json') as f:
-            event = json.loads(f.read())
-        self.assertFalse(is_back(event, F=(-158, -786), width=5))
+        self.assertFalse(is_back(self.read_event('fixtures/success.json'), F=self.F, width=5))
 
     def test_back_failure_312(self):
         # 5:12頃の影潜りの方向指定失敗してるイベント
-        with open('fixtures/failure.json') as f:
-            event = json.loads(f.read())
-        self.assertFalse(is_back(event, F=(-158, -786)))
+        self.assertFalse(is_back(self.read_event('fixtures/failure.json'), F=self.F))
 
 
 if __name__ == '__main__':
